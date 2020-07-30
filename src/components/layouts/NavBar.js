7c9cc761dbe6028f0cacb0ed9b12431c1e7ac385
= <=>
2020-07-30 16:45:53 +0800

    removing unnecessary comments

--- src/components/layouts/NavBar.js
+++ src/components/layouts/NavBar.js
@@ -28,9 +28,6 @@ export default function Navbar(props) {
         <div className={classes.root}>
             <AppBar position="static">
                 <Toolbar>
-                    {/*<IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
-                        <MenuIcon />
-                    </IconButton>*/}
                     <Typography variant="h6" className={classes.title}>
                         React Sample Demo
                     </Typography>
